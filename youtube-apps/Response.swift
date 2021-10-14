eee779c4f4181518de5e8b03da79a284d77c63d9
iserve121 <khadar.awre@gmail.com>
2021-10-14 15:13:30 +0100

    added code to parse the json

---
+++ youtube-apps/Response.swift
@@ -0,0 +1,28 @@
+//
+//  Response.swift
+//  youtube-apps
+//
+//  Created by A Roble on 14/10/2021.
+//
+
+import Foundation
+
+
+struct Response: Decodable {
+    
+    var items:[Video]?
+    
+    enum CodingKeys:String, CodingKey {
+        
+        case items
+        
+    }
+    
+    init (from decoder:Decoder) throws {
+        
+      let contianer = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.items = try contianer.decode([Video].self, forKey: .items)
+        
+    }
+}
