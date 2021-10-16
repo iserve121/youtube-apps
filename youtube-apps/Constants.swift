4422fed4d2b731ae30209fc5ec3ebc2bdc78aaa7
iserve121 <khadar.awre@gmail.com>
2021-10-16 13:01:34 +0100

    modified git ignore to tracking constantstemplate

--- youtube-apps/Constants.swift
+++ youtube-apps/Constants.swift
@@ -1,23 +1,20 @@
 //
-//  ConstantsTemplate.swift
+//  Constants.swift
 //  youtube-apps
 //
-//  Created by A Roble on 15/10/2021.
+//  Created by A Roble on 14/10/2021.
 //
-//
-/*
-
-Add your own keys below and uncomment the code
-*/
 
 import Foundation
 
+
 struct Constants {
     
-    static var API_KEY = ""
-    static var PLAYLIST_ID = ""
+    static var API_KEY = "AIzaSyCMpXMK3jykqstbiS_o9C0mmQLh9nnxqxI"
+    static var PLAYLIST_ID = "UUi3SVQgYgPiGVfU78FKgxcA"
     static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
- 
- static var VIDEOCELL_ID = "VideoCell"
+
+    static var VIDEOCELL_ID = "VideoCell"
 }
+
 
